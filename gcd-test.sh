c2d37adfe6bba06ab32f32d19838da129b3a8fec
Master <master@Masters-MacBook-Pro.local>
2019-06-04 18:58:34 +0900

    Add scripts.

---
+++ gcd-test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo 'CI OK'
---
+++ gcd.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# usage
+function usage {
+cat <<_EOT_
+Usage:
+  $0
+
+Description:
+  ２つの自然数の最大公約数を求めます。
+
+Options:
+  -h    マニュアルを表示
+
+_EOT_
+exit 1
+}
+
+# -h オプションが指定されたらusageを表示
+if [ "$1" == "-h" ]; then
+  usage
+  exit 0
+fi
+
+echo '2つの自然数をスペース区切りで入力してください'
+
+# 入力パラメータを読み込む
+read a b
+
+# 引数のチェック
+if [ -z $a -o -z $b ]; then
+ echo "実行するにはスペース区切りで2個の自然数が必要です。" 1>&2
+ exit 1
+fi
+
+m=$a
+
+if [ $b -lt $m ]
+then
+  m=$b
+fi
+
+# 最大公約数を求める
+while [ $m -ne 0 ]
+do
+  x=`expr $a % $m`
+  y=`expr $b % $m`
+
+  if [ $x -eq 0 -a $y -eq 0 ]; then
+    # Printing the greatest gcd of two given number
+    echo "$a と $b の最大公約数は $m  です"
+    break
+  fi
+  m=`expr $m - 1`
+done
