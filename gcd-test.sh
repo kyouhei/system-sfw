f48b4cfa9950134e74d3796a718fd0ef098490cb
Kyohei <kyouhei.maeda@me.com>
2019-06-04 20:57:24 +0900

    fail check.

--- gcd-test.sh
+++ gcd-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+exit 1
+echo '2と22の最大公約数は2'
 x=$( yes ' 2 22' | (./gcd.sh) )
 if [ $x = 2 ]; then
   echo 'OK'
@@ -8,6 +9,7 @@ else
   exit 1
 fi
 
+echo '3と13の最大公約数は9ではない'
 x=$( yes ' 3 13' | (./gcd.sh) )
 if [ ! $x = 9 ]; then
   echo 'OK'
