be58a2e150210a614fc1179d4a2066c41f5968ff
Kyohei <kyouhei.maeda@me.com>
2019-06-04 20:55:36 +0900

    add a test.

--- gcd-test.sh
+++ gcd-test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 x=$( yes ' 2 22' | (./gcd.sh) )
-if [ x==2 ]; then
+if [ $x = 2 ]; then
+  echo 'OK'
+else
+  echo 'NG'
+  exit 1
+fi
+
+x=$( yes ' 3 13' | (./gcd.sh) )
+if [ ! $x = 9 ]; then
   echo 'OK'
 else
   echo 'NG'
